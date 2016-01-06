e85a68c68a52ae88336368a849bee468e241b9b1
xianghongchun <1012995746@qq.com>
2016-01-06 10:18:36 +0800

    第一次修改
    春修改了

---
+++ Sprite.lua
@@ -0,0 +1,14 @@
+Sprite={x=0,y=0}--默认构造函数
+function Sprite.SetPosition( self,x,y)		
+	self.x=x
+	self.y=y
+end
+function Sprite.Show( self )
+	print(self.x)
+	print(self.y)
+end
+function Sprite:move(  )
+	self.x=self.x+10
+	self.y=self.y+10
+end
+return Sprite
---
+++ Spritemain.lua
@@ -0,0 +1,6 @@
+sprite=require("a")
+sprite.SetPosition(sprite,100,100)
+sprite.Show(sprite)--100 100
+
+sprite:move()
+sprite:Show()--110 110
